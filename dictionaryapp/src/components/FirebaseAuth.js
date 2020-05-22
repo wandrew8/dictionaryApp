9078b59664d692d9c5bbfde75405082b986307f0
Andrew Weiss <wandrew8@gmail.com>
2020-05-22 09:35:36 -0700

    deploying app

--- dictionaryapp/src/components/FirebaseAuth.js
+++ dictionaryapp/src/components/FirebaseAuth.js
@@ -7,18 +7,18 @@ export default class Home extends React.Component {
         // Popup signin flow rather than redirect flow.
         signInFlow: 'popup',
         // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-        signInSuccessUrl: '/user',
+        // signInSuccessUrl: '/user',
         // We will display Google and Facebook as auth providers.
         signInOptions: [
           firebase.auth.EmailAuthProvider.PROVIDER_ID,
           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
           firebase.auth.GithubAuthProvider.PROVIDER_ID
         ],
-        callbacks: {
-            signInSuccessWithAuthResult: () => {
-                this.props.history.push('/user');
-            }
-        }
+        // callbacks: {
+        //     signInSuccessWithAuthResult: () => {
+        //         this.props.history.push('/user');
+        //     }
+        // }
       };
 
       render() {
